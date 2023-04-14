0c1e3d7a20ff23fdd31e7c8750238fb8359039ba
Mervynson Ramos <mervynson08@gmail.com>
2023-04-14 11:13:05 +0800

    page responsiveness
    Land.js
    Login.js
    Page404.js
    Signup.js

--- taskone/src/pages/Page404.js
+++ taskone/src/pages/Page404.js
@@ -7,15 +7,15 @@ export default function Page404() {
     navigate(-1);
   };
   return (
-    <div className="flex h-screen w-full">
-      <div className="flex items-center justify-center w-3/5">
-        <img className="w-full" src={logo} alt="taken" />
+    <div className="flex h-screen w-full flex-col lg:flex-row items-center sm:items-center">
+      <div className="flex items-center justify-center w-3/5 p-10">
+        <img className="w-[400px] sm:w-[400px] lg:w-full" src={logo} alt="taken" />
       </div>
-      <div className="flex flex-col items-center justify-center w-2/5 pr-24">
-        <span className="font-mont font-extrabold text-white text-9xl">
+      <div className="flex flex-col items-center justify-center w-2/5 pr-0 sm:pr-0 lg:pr-24">
+        <span className="font-mont font-extrabold text-white text-7xl sm:text-7xl lg:text-9xl">
           404
         </span>
-        <span className="font-mont font-extrabold text-white text-5xl">
+        <span className="font-mont font-extrabold text-white text-3xl sm:text-3xl lg:text-5xl sm:text-center p-5">
           Page Not Found
         </span>
         <p className="text-center text-white mt-8 mb-4">
